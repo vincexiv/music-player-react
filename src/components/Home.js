bb10ff1c925eab8284b5153ed1930bb95523e474
Vincent <makokhavomondi@gmail.com>
2022-10-05 14:09:55 +0300

    Create components for using Browser Router

---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React, {useContext} from "react";
+import { songDetails } from "../SongDetailsContextProvider";
+import { apiHost } from "../variables";
+import LeftItem from "./LeftItem";
+import RightItem from "./RightItem";
+import CenterItem from "./CenterItem";
+import Footer from "./Footer"
+
+
+function Home(){
+    const {activePlaylist, setActivePlaylist} = useContext(songDetails)
+  
+  
+    function handlePlaylistChange(newPlaylist){
+      if(newPlaylist === "favorites"){
+        fetch(`${apiHost}/musicInfo?_embed=comments&favorited=true`)
+          .then(result => result.json())
+          .then(data => setActivePlaylist({name: newPlaylist, data: data}))
+      }else{
+        fetch(`${apiHost}/musicInfo?_embed=comments&playList=${newPlaylist}`)
+          .then(result => result.json())
+          .then(data => setActivePlaylist({name: newPlaylist, data: data}))
+      }
+    }
+  
+  
+    return (
+      <>  
+        <section className="container">
+          <div id="main-content">
+            <LeftItem />
+            <CenterItem/>
+            <RightItem activePlaylist={activePlaylist} handlePlaylistChange={handlePlaylistChange} />
+          </div>
+        </section>
+  
+        <Footer />
+      </>
+  
+    )
+}
+
+
+export default Home
