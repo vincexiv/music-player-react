49f3588a5e4d770d7dac17bbc293b6ae090ff7cd
Vincent <makokhavomondi@gmail.com>
2022-10-05 22:35:25 +0300

    Enable adding comments to currently playing songs

--- src/components/CenterItemReactions.js
+++ src/components/CenterItemReactions.js
@@ -1,12 +1,42 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
+import { apiHost } from '../variables'
 import { songDetails } from '../SongDetailsContextProvider'
 import CommentListItem from './CenterItemReactionsComments'
 
 
 function CenterItemReactions(){
-    const {currentlyPlaying, setCurrentlyPlaying} = useContext(songDetails)
+    const {currentlyPlaying, userDetails, setCurrentlyPlaying} = useContext(songDetails)
+    const [newComment, setNewComment] = useState("")
 
     const commentListItems = currentlyPlaying.comments.map(comment => <CommentListItem key={comment.id} comment={comment}/>)
+
+    function handleNewComment(event){
+        setNewComment(event.target.value)
+    }
+
+    function handleCommentSubmit(event){
+        event.preventDefault()
+
+        console.log(userDetails)
+
+        fetch(`${apiHost}/comments`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+            body: JSON.stringify(
+                {
+                    playlist: currentlyPlaying.playList,
+                    musicInfoId: currentlyPlaying.id,
+                    commenterName: userDetails.userDetails.username,
+                    content: newComment
+                }
+            )
+        })
+        .then(result => result.json())
+        .then(data => {
+            setCurrentlyPlaying({...currentlyPlaying, comments: [...currentlyPlaying.comments, data]})
+        })
+    }
+
 
     return (
         <>
@@ -22,8 +52,8 @@ function CenterItemReactions(){
                 <p id="like-count"><span className="count">{currentlyPlaying.likes}</span> likes</p>
             </div>
             
-            <form id="add-comment-form">
-                <textarea cols="30" rows="10" placeholder="add comment"></textarea>
+            <form id="add-comment-form" onSubmit={(event) => handleCommentSubmit(event)}>
+                <textarea cols="30" rows="10" placeholder="add comment" value={newComment} onChange={(event)=>handleNewComment(event)}></textarea>
                 <input type="submit" className="btn" value="submit" />
             </form>        
         </>        
